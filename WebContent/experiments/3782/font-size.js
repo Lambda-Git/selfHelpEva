5bdc658ab68bef916f1dd0be8daa9d41486ad2ba
Lambda-Git <867831117@qq.com>
2020-03-31 20:23:05 +0800

    修改问题

--- WebContent/experiments/3782/font-size.js
+++
@@ -1,7 +0,0 @@
-
-	function fontsetting(){
-		//修改数字即可字体大小 ，contentText对应问题； radioA对应左侧答案 ；radioB 对应右侧答案
-		$("#contentText").css("font-size","1em");
-		$("#radioA").css("font-size","1.5vw");
-		$("#radioB").css("font-size","1.5vw");
-	}
--- WebContent/js/font-size.js
+++ WebContent/js/font-size.js
@@ -2,6 +2,7 @@
 	function fontsetting(){
 		//修改数字即可字体大小 ，contentText对应问题； radioA对应左侧答案 ；radioB 对应右侧答案
 		$("#contentText").css("font-size","1em");
-		$("#radioA").css("font-size","1.5vw");
-		$("#radioB").css("font-size","1.5vw");
+		$("#radioA").css("font-size","2.5vw");
+		$("#radioB").css("font-size","2.5vw");
+		$("#textArea img").css("max-width","350px");
 	}
