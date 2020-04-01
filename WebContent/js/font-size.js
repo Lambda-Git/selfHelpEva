863f6946e00a80c330081c3740fb39aa683a8aad
周颖瑜 <zhouyingyu@payingcloud.cn>
2020-04-01 17:19:14 +0800

    压缩文件

--- WebContent/js/font-size.js
+++ WebContent/js/font-size.js
@@ -2,7 +2,10 @@
 	function fontsetting(){
 		//修改数字即可字体大小 ，contentText对应问题； radioA对应左侧答案 ；radioB 对应右侧答案
 		$("#contentText").css("font-size","1em");
-		$("#radioA").css("font-size","2.5vw");
-		$("#radioB").css("font-size","2.5vw");
+		$("#radioA").css("font-size","2.5em");
+		$("#radioA").css("line-height","1.2em");
+		$("#radioB").css("font-size","2.5em");
+		$("#radioB").css("line-height","1.2em");
 		$("#textArea img").css("max-width","350px");
+		$("#textArea img").css("max-height","180px");
 	}
