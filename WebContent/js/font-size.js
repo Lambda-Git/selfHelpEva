846b08ea67399907203e9c8182a3336ba2aedd79
Lambda-Git <867831117@qq.com>
2020-04-15 11:10:57 +0800

    欢迎 realName  修改  6步指导语 按钮字体调大

--- WebContent/js/font-size.js
+++ WebContent/js/font-size.js
@@ -2,10 +2,10 @@
 	function fontsetting(){
 		//修改数字即可字体大小 ，contentText对应问题； radioA对应左侧答案 ；radioB 对应右侧答案
 		$("#contentText").css("font-size","1em");
-		$("#radioA").css("font-size","1.5em");
-		$("#radioA").css("line-height","1.2em");
-		$("#radioB").css("font-size","1.5em");
-		$("#radioB").css("line-height","1.2em");
+		$("#radioA").css("font-size","20px");
+		$("#radioA").css("line-height","20px");
+		$("#radioB").css("font-size","20px");
+		$("#radioB").css("line-height","20px");
 		$("#textArea img").css("max-width","350px");
 		$("#textArea img").css("max-height","180px");
 	}
